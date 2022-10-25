443efa6e8e74adc068cecb012ad69854d898d6dd
Nonz6274 <105692989+Nonz6274@users.noreply.github.com>
2022-10-25 10:13:02 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import LiveChat from "react-livechat";
+
+import "./App.css";
+
+const App = () => {
+  return <div>{/* <LiveChat license={12026424} /> */}</div>;
+};
+
+export default App;
